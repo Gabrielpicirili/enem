5a22b2c67685c4131c7a8a707d74d7dbc41ace51
Gabrielpicirili <gabriel.picirili@aluno.senai.br>
2024-02-28 09:22:25 -0300

    first commit

---
+++ src/screens/Categorias/index.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import styles from "./styles.js";
+
+export default function Profile() {
+  return (
+    <View styles={styles.container}>
+      <Text>Profile</Text>
+    </View>
+  );
+}
